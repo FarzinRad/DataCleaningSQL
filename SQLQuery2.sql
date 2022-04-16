4ab74086ec1b24bac4ef2fe8a49410d9d5b851d7
FarzinRad <Farzinvado@yahoo.com>
2022-04-16 12:41:34 +0430

    Remove Duplicate

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -138,7 +138,27 @@ From DataCleaningHousing.dbo.NashvilleHousing
 -- Change Y and N to Yes and No in "Sold as Vacant" field
 
 
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From DataCleaningHousing.dbo.NashvilleHousing
+Group by SoldAsVacant
+order by 2
 
+
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From DataCleaningHousing.dbo.NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
 
 
 
@@ -148,7 +168,30 @@ From DataCleaningHousing.dbo.NashvilleHousing
 
 -- Remove Duplicates
 
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
 
+From PortfolioProject.dbo.NashvilleHousing
+--order by ParcelID
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
 
 
 
